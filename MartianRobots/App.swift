4253d9532be822f8ccb95309867cc3c874226453
Neetesh Gupta <neetesh.ng@gmail.com>
2017-10-22 21:52:20 +0100

    Add basic logic of running all robots

--- MartianRobots/App.swift
+++ MartianRobots/App.swift
@@ -5,6 +5,37 @@ struct App {
     let sequence: [RobotSequence]
 
     func run() {
+        var scentLocations = [Location]()
 
+        for robotSequence in sequence {
+            var robot = robotSequence.robot
+            let instructions = robotSequence.instructions
+            var isLost = false
+
+            for instruction in instructions {
+                let location = robot.location
+
+                switch instruction {
+                case .L:
+                    robot.turnLeft()
+                case .R:
+                    robot.turnRight()
+                case .F:
+                    robot.moveForward()
+                }
+
+                if !surface.contains(location: robot.location) {
+                    isLost = true
+                    scentLocations.append(location)
+                }
+            }
+
+            print ("\(robot.location.x) \(robot.location.y) \(robot.orientation)", terminator:"")
+            if isLost {
+                print(" LOST")
+            } else {
+                print("")
+            }
+        }
     }
 }
